1cc367b1ca3f70d2e9287a92683ea3c70854b0fd
Agon_cp <SND\Agon_cp>
2013-01-28 10:39:13 +0000

    + Revamp of theme source control - working progress + Metro - new theme - working progress v0.03

---
+++ FamilyLinesThemeExperimental/FamilyLines/Controls/TaskBar.cs
@@ -0,0 +1,90 @@
+﻿using System.Windows;
+using System.Windows.Shell;
+
+namespace KBS.FamilyLines.Controls
+{
+    /// <summary>
+    /// KBR 12/31/2012 - Originally a helper class for the WindowsAPICodePack Taskbar manager, I've done a 
+    /// Q&D rework to use the .NET 4.0 support instead.
+    /// TODO: make this disappear completely in a future release.
+    /// </summary>
+    internal class TaskBar
+    {
+        #region Properties
+
+        public static TaskBar Current { get; private set; }
+
+        private TaskbarItemInfo TBI { get { return _window.TaskbarItemInfo; } }
+
+        #endregion
+
+        #region Initialization
+
+        private TaskBar()
+        {
+        }
+
+        private JumpList _jumpList;
+        private Window _window;
+
+        private TaskBar(Window mainWindow, string appId)
+        {
+            _window = mainWindow;
+
+            if (_window.TaskbarItemInfo == null)
+            {
+                var tbi = new TaskbarItemInfo {Description = "FamilyLines Genealogy Program"};
+                _window.TaskbarItemInfo = tbi;
+            }
+
+            _jumpList = new JumpList();
+        }
+
+        /// <summary>
+        /// Creates a single task bar instance.
+        /// </summary>
+        /// <returns></returns>
+        public static TaskBar Create(Window mainWindow, string appId, params JumpTask[] tasks)
+        {
+            Current = new TaskBar(mainWindow, appId);
+
+            // NOTE: order is important!!! The tasks must be added BEFORE SetJumpList is called!!
+            Current._jumpList.JumpItems.AddRange(tasks);
+
+            JumpList.SetJumpList(Application.Current, Current._jumpList);
+            return Current;
+        }
+
+        #endregion
+
+        #region Operations
+
+        /// <summary>
+        /// Set the progress of a Windows 7 taskbar.
+        /// </summary>
+        /// <param name="progress"></param>
+        public virtual void Progress(int progress)
+        {
+            Current.TBI.ProgressValue = progress;
+            Current.TBI.ProgressState = TaskbarItemProgressState.Normal;
+        }
+
+        /// <summary>
+        /// Set the progress of a Windows 7 taskbar to indeterminate.
+        /// </summary>
+        public virtual void Loading()
+        {
+            Current.TBI.ProgressState = TaskbarItemProgressState.Indeterminate;
+        }
+
+        /// <summary>
+        /// Clear the progress of a Windows 7 taskbar.
+        /// </summary>
+        public virtual void Restore()
+        {
+            Current.TBI.ProgressState = TaskbarItemProgressState.None;
+        }
+
+        #endregion
+    }
+}
---
+++ FamilyLinesThemeExperimental/FamilyLinesLib/CustomDateTime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KBS.FamilyLinesLib
+{
+    class CustomDateTime
+    {
+        enum MyEnum
+        {
+            
+        }
+
+        private int Year;
+        private byte Month;
+        private short Day;
+
+        private byte Hour;
+        private byte Minute;
+        private byte Second;
+        private byte Millisecond;
+
+        private bool isAD = true;
+
+        public CustomDateTime()
+        {
+                
+        }
+
+    }
+}
