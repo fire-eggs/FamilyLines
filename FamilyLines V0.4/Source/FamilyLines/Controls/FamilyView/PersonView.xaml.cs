ec7d3c201c8997e5718d14054b28dcedb1ed1799
notonyournelly_cp <SND\notonyournelly_cp>
2013-05-09 15:52:00 +0000

    Family Lines 0.4 branch

---
+++ FamilyLines V0.4/Source/FamilyLines/Controls/FamilyView/PersonView.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using KBS.FamilyLinesLib;
+
+namespace KBS.FamilyLines.Controls.FamilyView
+{
+    /// <summary>
+    /// Interaction logic for PersonView.xaml
+    /// </summary>
+    public partial class PersonView : INotifyPropertyChanged
+    {
+        #region Properties
+
+        private Person _human;
+        public Person Human
+        {
+            get { return _human; }
+            set
+            {
+                _human = value;
+
+                UpdateSpouseList();
+
+                OnPropertyChanged("Human");
+                OnPropertyChanged("HasMoreSpouse");
+            }
+        }
+
+        public bool Child { get; set; }
+
+        public int SpouseColumn { set; get; }
+
+        public bool HasMoreSpouse
+        {
+            get
+            {
+                return _human != null && _human.Spouses.Count > 1;
+            }
+        }
+        #endregion
+
+        public PersonView()
+        {
+            InitializeComponent();
+
+            // TODO attempts to set datacontext in XAML have been spotty...
+            DataContext = this;
+            SpouseNav.DataContext = this;
+
+            SpouseColumn = 0;
+        }
+
+        private void UpdateSpouseList()
+        {
+            SpouseList.Items.Clear();
+            if (Human == null)
+                return;
+
+            // TODO building these on the fly, getting "cannot find source for binding" messages. Consider using ItemTemplate+ItemsSource?
+
+            // 1. Create 'Add new spouse' entry
+            var cbi = new ComboBoxItem();
+            cbi.Content = Properties.Resources.AddNewSpouse;
+            cbi.Selected += addSpouse_Selected;
+            SpouseList.Items.Add(cbi);
+
+            // 2. For each spouse, add an entry
+            foreach (Relationship rel in Human.Relationships)
+            {
+                if (rel.RelationshipType == RelationshipType.Spouse)
+                {
+                    var spouse = rel as SpouseRelationship;
+                    if (spouse == null)
+                        continue;
+                    var cbiS = new ComboBoxItem();
+                    cbiS.Content = spouse.PersonFullName;
+                    cbiS.DataContext = spouse; // TODO better place to store this?
+                    cbiS.Selected += gotoSpouse_Selected;
+
+                    SpouseList.Items.Add(cbiS);
+                }
+            }
+        }
+
+        private void addSpouse_Selected(object sender, RoutedEventArgs e)
+        {
+            // Fire an 'add spouse' event
+            var e2 = new RoutedEventArgs(Commands.AddSpouse, Human);
+            RaiseEvent(e2);
+        }
+
+        private void gotoSpouse_Selected(object sender, RoutedEventArgs e)
+        {
+            var selectedSpouse = (sender as ComboBoxItem).DataContext as SpouseRelationship;
+            var spouse = selectedSpouse.RelationTo;
+            var spouseProps = new Tuple<Person, Person>(Human, spouse);
+            var e2 = new RoutedEventArgs(Commands.ViewSpouse, spouseProps);
+            RaiseEvent(e2);
+        }
+
+        private void doTooltip(object sender, string format, string param)
+        {
+            var b = sender as FrameworkElement;
+            if (b == null)
+                return;
+            var t = b.ToolTip as string;
+            if (t == null)
+                return;
+            b.ToolTip = String.Format(format, param);
+        }
+
+        private void go(Person aperson)
+        {
+            App.Family.Current = aperson;
+        }
+
+        #region Event Handlers
+
+        private void Border_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (Human != null)
+                go(Human);
+        }
+
+        private void Border_ToolTipOpening(object sender, ToolTipEventArgs e)
+        {
+            if (Human != null)
+                doTooltip(sender, Properties.Resources.ClickToMakeCurrent, Human.FullName);
+        }
+
+        private void SpouseList_ToolTipOpening(object sender, ToolTipEventArgs e)
+        {
+            //if (Human != null)
+            //    doTooltip(sender, "View or add spouse for {0}", Human.FullName);
+        }
+
+        #endregion
+
+        #region Implementation of INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+    }
+}
---
+++ FamilyLines V0.4/Source/FamilyLinesLib/Photo.cs
@@ -0,0 +1,213 @@
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace KBS.FamilyLinesLib
+{
+    /// <summary>
+    /// Simple representation of a serializable photo associated with the Person class
+    /// </summary>
+    [Serializable]
+    public class Photo : INotifyPropertyChanged
+    {
+        #region Fields and Constants
+
+        public const string PhotosFolderName = "Images";
+        private string relativePath;
+        private bool isAvatar;
+        
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// The relative path to the photo.
+        /// </summary>
+        public string RelativePath
+        {
+            get { return relativePath; }
+            set
+            {
+                if (relativePath != value)
+                {
+                    relativePath = value;
+                    OnPropertyChanged("relativePath");
+                }
+            }
+        }
+
+        // KBR 03/18/2012 Apply the essence of patch 1784 by PandaWood.
+		public static string FamilyFilePath;
+		public static void SetFamilyFileNamePath(string familyFilePath)
+		{
+			FamilyFilePath = familyFilePath;
+		}
+
+        /// <summary>
+        /// The fully qualified path to the photo.
+        /// </summary>
+        [XmlIgnore]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "value")]
+        public string FullyQualifiedPath
+        {
+            get
+            {
+
+                string tempFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                App.ApplicationFolderName);
+                tempFolder = Path.Combine(tempFolder, App.AppDataFolderName);
+
+                var path1 = Path.Combine(tempFolder, relativePath);
+
+                // KBR 03/18/2012 Apply the essence of patch 1784 by PandaWood. If the fully qualified path doesn't exist,
+                // use the relativepath. This allows transporting a "Family.Show" package, with photos, to other people.
+                // I believe this works only with "Version2" format, not the "V3" format???
+                if (File.Exists(path1))
+                    return path1;
+                if (FamilyFilePath == null)
+                    return relativePath;
+			    return Path.Combine(FamilyFilePath, relativePath);
+            }
+            set
+            {
+                // This empty setter is needed for serialization.
+            }
+        }
+
+        /// <summary>
+        /// Whether the photo is the avatar photo or not.
+        /// </summary>
+        public bool IsAvatar
+        {
+            get { return isAvatar; }
+            set
+            {
+                if (isAvatar != value)
+                {
+                    isAvatar = value;
+                    OnPropertyChanged("IsAvatar");
+                }
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Empty constructor is needed for serialization
+        /// </summary>
+        public Photo() { }
+
+        /// <summary>
+        /// Constructor for Photo. Copies the photoPath to the images folder
+        /// </summary>
+        public Photo(string photoPath)
+        {
+
+            if (!string.IsNullOrEmpty(photoPath))
+                // Copy the photo to the images folder
+                this.relativePath = Copy(photoPath);
+        }
+
+        #endregion
+
+        #region Methods
+        
+        public override string ToString()
+        {
+            return FullyQualifiedPath;
+        }
+
+        /// <summary>
+        /// Copies the photo file to the application photos folder. 
+        /// Returns the relative path to the copied photo.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+        private static string Copy(string fileName)
+        {
+		
+            // The photo file being copied
+            FileInfo fi = new FileInfo(fileName);
+
+            // Absolute path to the application folder
+            string appLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                App.ApplicationFolderName);
+            appLocation = Path.Combine(appLocation, App.AppDataFolderName);
+
+            // Absolute path to the photos folder
+            string photoLocation = Path.Combine(appLocation, PhotosFolderName);
+
+            // Fully qualified path to the new photo file
+            string photoFullPath = Path.Combine(photoLocation, App.ReplaceEncodedCharacters(fi.Name));
+
+            // Relative path to the new photo file
+		
+			string photoRelLocation = Path.Combine(PhotosFolderName, App.ReplaceEncodedCharacters(fi.Name));
+
+            // Create the appLocation directory if it doesn't exist
+            if (!Directory.Exists(appLocation))
+                Directory.CreateDirectory(appLocation);
+
+            // Create the photos directory if it doesn't exist
+            if (!Directory.Exists(photoLocation))
+                Directory.CreateDirectory(photoLocation);
+            
+            // Copy the photo.
+            try
+            {
+                string photoName = Path.GetFileName(photoFullPath);
+                string photoNameNoExt = Path.GetFileNameWithoutExtension(photoFullPath);
+                string photoNameExt = Path.GetExtension(photoFullPath);
+
+                int i = 1;
+
+                if (File.Exists(photoFullPath))
+                {
+                    do
+                    {
+                        photoName = photoNameNoExt + "(" + i + ")" + photoNameExt;  //don't overwrite existing files, append (#) to file if exists.
+                        photoRelLocation = Path.Combine(PhotosFolderName, photoName);
+                        photoFullPath = Path.Combine(photoLocation, photoName);
+                        i++;
+                    }
+                    while (File.Exists(photoFullPath));
+                    
+                }
+                fi.CopyTo(photoFullPath, true);
+
+            }
+            catch
+            {
+                // Could not copy the photo. Handle all exceptions 
+                // the same, ignore and continue.
+            }
+
+            return photoRelLocation;
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Collection for photos.
+    /// </summary>
+    [Serializable]
+    public class PhotoCollection : ObservableCollection<Photo>
+    {
+    }
+}
